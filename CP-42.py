e200c53aaf46d0d1bfe3c6b7ef69f746ebe89694
BaranovNiDm <114215520+BaranovNiDm@users.noreply.github.com>
2022-10-12 21:46:19 +0300

    Update CP-42.py

--- CP-42.py
+++ CP-42.py
@@ -2,13 +2,15 @@ print("введите длину первого массива")
 lena=int(input())
 print("введите длину второго массива")
 lenb=int(input())
-print("введите 4элементы первого массива через пробел")
+print("введите элементы первого массива через пробел")
 a=[float(i) for i in input().split()]
 print("введите элементы второго массива через пробел")
 b=[float(i) for i in input().split()]
 
+#введем новый массив для одинаковых чисел
 ab=[]
 
+#ищем одинаковые элементы и вводим их в ab
 for i in range(len(a)):
     if a[i] in b and a[i] not in ab:
         ab.append(a[i])
