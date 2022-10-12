e2b2c0f549c24d5435dd698840f331215acec9bc
NikitaBaranov <dimawarriors3qazwsx@gmail.com>
2022-10-12 21:13:33 +0300

    zad1

---
+++ CP-4.py
@@ -0,0 +1,16 @@
+pip install pylint
+print("введите размерность массива")
+lenn=int(input())
+print("введите элементы массива через пробел(колличество элементов равно размерности массива)1")
+a=[float(i) for i in input().split()]
+print(*a)
+maxx=0
+k=0
+for i in range(0,lenn):
+    if (a[i])>maxx:
+        maxx=a[i]
+        num=k
+    k+=1
+for i in range(num+1,lenn):
+    a[i]=0
+print(a)
