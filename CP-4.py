63bf1e910c995bcfe8d1f5d13e3847d6e605d6eb
BaranovNiDm <114215520+BaranovNiDm@users.noreply.github.com>
2022-10-12 21:32:02 +0300

    Update CP-4.py

--- CP-4.py
+++ CP-4.py
@@ -2,7 +2,8 @@ print("введите размерность массива")
 lenn=int(input())
 print("введите элементы массива через пробел(колличество элементов равно размерности массива)1")
 a=[float(i) for i in input().split()]
-print(*a)
+
+#найдем номер максимального числа в массиве
 maxx=0
 k=0
 for i in range(0,lenn):
@@ -10,6 +11,8 @@ for i in range(0,lenn):
         maxx=a[i]
         num=k
     k+=1
+
+#изменим все числа после этого номера и выведем их на экран
 for i in range(num+1,lenn):
     a[i]=0
-print(a)
+print(*a)
