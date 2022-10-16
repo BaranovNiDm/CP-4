0a42c3ab35eb3c401c5778c498b7a896b447c50a
BaranovNiDm <114215520+BaranovNiDm@users.noreply.github.com>
2022-10-16 22:29:25 +0300

    Update CP-43.py

--- CP-43.py
+++ CP-43.py
@@ -1,9 +1,16 @@
+#введите члены последовательности через пробел
 a=[float(i) for i in input().split()]
 Delta = float(input())
 count=0
 
+#найдем минимальный член член множества
+minn=99999999999999999999999999999999999999999999
+for i in range(len(a)):
+    if a[i]<minn:
+        minn=a[i]
+    
 #найдеи кол-во отличающихся от минимального на Delta
 for i in range(len(a)):
-    if a[i]-min(a)==Delta:
+    if a[i]-minn==Delta:
         count+=1
 print(count)
